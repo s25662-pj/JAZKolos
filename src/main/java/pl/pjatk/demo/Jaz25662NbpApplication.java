33794be16d5eef27928df382adc1d6d815d3e8a4
s25662-pj <s25662@pjwstk.edu.pl>
2023-06-24 10:10:26 +0200

    pierwszy

---
+++ src/main/java/pl/pjatk/demo/Jaz25662NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz25662NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz25662NbpApplication.class, args);
+	}
+
+}
